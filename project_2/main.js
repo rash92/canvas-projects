cd81fcb76d4442f50fccf514b79aa7d212e7b509
rrahman <rashidur92@hotmail.com>
2024-11-29 17:02:41 +0000

    added multiple players, simple random AI. Next task is food

--- project_2/main.js
+++ project_2/main.js
@@ -1,59 +1,92 @@
-import { Player } from "./player.js"
+import { AiSnake, Keyboard1, Keyboard2 } from "./player.js";
 
 class Game {
-    constructor(canvas, context){
-        this.canvas = canvas
-        this.ctx = context
-        this.width
-        this.height
-        this.snake = new Player(this,0,0, 1, 1)
-        this.cellSize = 50
-        this.columns
-        this.rows
+    constructor(canvas, context) {
+        this.canvas = canvas;
+        this.ctx = context;
+        this.width;
+        this.height;
+        this.cellSize = 50;
+        this.columns;
+        this.rows;
+        this.eventTimer = 0;
+        this.eventInterval = 200;
+        this.eventUpdate = false;
+        this.player1
+        this.player2
+        this.gameObjects
 
-        window.addEventListener('resize', e => {
-            this.resize(e.currentTarget.innerWidth, e.currentTarget.innerHeight)
+        window.addEventListener("resize", (e) => {
+            this.resize(e.currentTarget.innerWidth, e.currentTarget.innerHeight);
+        });
+        this.resize(window.innerWidth, window.innerHeight);
+    }
+    resize(width, height) {
+        this.canvas.width = width - (width % this.cellSize);
+        this.canvas.height = height - (height % this.cellSize);
+        this.width = this.canvas.width;
+        this.height = this.canvas.height;
+        this.columns = Math.floor(this.width / this.cellSize);
+        this.rows = Math.floor(this.height / this.cellSize);
+        this.player1 = new Keyboard1(this, 0, 0, 0, 1, 'red');
+        this.player2 = new Keyboard2(this,  this.columns - 1,this.rows - 1, 0, -1, 'blue')
+        this.player3 = new AiSnake(this, 0, this.rows - 1, 1, 0, 'black')
+        this.player4 = new AiSnake(this,  this.columns - 1,0, -1, 0, 'white')
+        this.gameObjects = [this.player1, this.player2, this.player3, this.player4]
+        // this.snake.resize()
+    }
+    handlePeriodicEvents(deltaTime) {
+        if (this.eventTimer < this.eventInterval) {
+            this.eventTimer += deltaTime;
+            this.eventUpdate = false;
+        } else {
+            this.eventTimer = 0;
+            this.eventUpdate = true;
+        }
+    }
+    render(deltaTime) {
+        this.handlePeriodicEvents(deltaTime);
+        if (this.eventUpdate) {
+            this.ctx.clearRect(0, 0, this.width, this.height);
+            this.drawGrid();
 
-        })
-        this.resize(window.innerWidth,window.innerHeight)
+            this.gameObjects.forEach((object)=>{
+                object.draw()
+                object.update()
+            })
+
+        }
     }
-    resize(width, height){
-        this.canvas.width = width - width%this.cellSize
-        this.canvas.height = height - height%this.cellSize
-        this.width = this.canvas.width
-        this.height = this.canvas.height
-        this.columns = this.width/this.cellSize
-        this.rows = this.height/this.cellSize
-        this.render()
-    }
-    render(){
-        this.drawGrid()
-        this.snake.update()
-        this.snake.draw()
-    }
-    drawGrid(){
-        console.log("trying to draw grid")
-        for (let y = 0; y < this.rows; y++){
-            for (let x = 0; x < this.columns; x++){
-                this.ctx.strokeRect(x*this.cellSize,y*this.cellSize,this.cellSize,this.cellSize)
+    drawGrid() {
+        for (let y = 0; y < this.rows; y++) {
+            for (let x = 0; x < this.columns; x++) {
+                this.ctx.strokeRect(
+                    x * this.cellSize,
+                    y * this.cellSize,
+                    this.cellSize,
+                    this.cellSize
+                );
             }
         }
     }
 }
 
-window.addEventListener('load', ()=>{
-    const canvas = document.getElementById('canvas1')
-    const ctx = canvas.getContext('2d')
-    canvas.width = window.innerWidth
-    canvas.height = window.innerHeight
+window.addEventListener("load", () => {
+    const canvas = document.getElementById("canvas1");
+    const ctx = canvas.getContext("2d");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 
-    const game = new Game(canvas, ctx)
-    game.render()
+    const game = new Game(canvas, ctx);
+    game.render();
 
-    function animate(){
-        ctx.clearRect(0,0,canvas.width,canvas.height)
-        game.render()
-        requestAnimationFrame(animate)
+    let lastTime = 0;
+    function animate(timeStamp) {
+        const deltaTime = timeStamp - lastTime;
+        lastTime = timeStamp;
+
+        game.render(deltaTime);
+        requestAnimationFrame(animate);
     }
-    requestAnimationFrame(animate)
-})
+    requestAnimationFrame(animate);
+});
--- project_2/player.js
+++ project_2/player.js
@@ -1,22 +1,126 @@
-export class Player {
-    constructor(game, x, y, vx, vy){
-        this.game = game
-        this.x = x
-        this.y = y
-        this.vx = vx
-        this.vy = vy
+export class Snake {
+    constructor(game, x, y, vx, vy, color) {
+        this.game = game;
+        this.x = x;
+        this.y = y;
+        this.vx = vx;
+        this.vy = vy;
+        this.width = this.game.cellSize;
+        this.height = this.game.cellSize;
+        this.color = color;
+        this.moving = true;
+    }
+    update() {
+        // stop moving when hitting walls, alternatively can change to dying
+        if (this.x <= 0 && this.vx < 0 || this.y <= 0 && this.vy < 0 || this.x >= this.game.columns - 1 && this.vx > 0 || this.y >= this.game.rows - 1 && this.vy > 0) {
+            this.moving = false
+        }
+        if (this.moving) {
+            this.x += this.vx;
+            this.y += this.vy;
+        }
+    }
+    draw() {
+        this.game.ctx.fillStyle = this.color;
+        this.game.ctx.fillRect(
+            this.x * this.game.cellSize,
+            this.y * this.game.cellSize,
+            this.width,
+            this.height
+        );
+    }
+    turnUp() {
+        this.vx = 0;
+        this.vy = -1;
+        this.moving = true
+    }
+    turnDown() {
+        this.vx = 0;
+        this.vy = 1;
+        this.moving = true
 
+    }
+    turnLeft() {
+        this.vx = -1;
+        this.vy = 0;
+        this.moving = true
+
+    }
+    turnRight() {
+        this.vx = 1;
+        this.vy = 0;
+        this.moving = true
+
+    }
+}
+
+
+export class Keyboard1 extends Snake {
+    constructor(game, x, y, vx, vy, color) {
+        super(game, x, y, vx, vy, color);
+
+        window.addEventListener("keydown", (e) => {
+            switch (e.key.toLowerCase()) {
+                case "w":
+                    this.turnUp();
+                    break;
+                case "s":
+                    this.turnDown();
+                    break;
+                case "a":
+                    this.turnLeft();
+                    break;
+                case "d":
+                    this.turnRight();
+            }
+        });
+    }
+}
+
+export class Keyboard2 extends Snake {
+    constructor(game, x, y, vx, vy, color) {
+        super(game, x, y, vx, vy, color);
+
+        window.addEventListener("keydown", (e) => {
+            switch (e.key) {
+                case "ArrowUp":
+                    this.turnUp();
+                    break;
+                case "ArrowDown":
+                    this.turnDown();
+                    break;
+                case "ArrowLeft":
+                    this.turnLeft();
+                    break;
+                case "ArrowRight":
+                    this.turnRight();
+            }
+        });
+    }
+}
+
+export class AiSnake extends Snake {
+    constructor(game, x, y, vx, vy, color) {
+        super(game, x, y, vx, vy, color);
+        this.turnTimer = 0
+        this.turnInterval = Math.floor(Math.random()*5)
+        
     }
     update(){
-        this.x += this.vx
-        this.y += this.vy
-    }
-    draw(){
-        this.game.ctx.fillStyle = 'blue'
-        this.game.ctx.fillRect(this.x,this.y,50,20)
+        super.update()
+        if (this.turnTimer < this.turnInterval){
+            this.turnTimer += 1
+        } else {
+            this.turnTimer = 0
+            this.turnRandom()
+        }
 
     }
-    resize(){
-
+    turnRandom(){
+        if (this.vy === 0){
+            Math.random() < 0.5 ? this.turnUp() : this.turnDown()
+        }else if (this.vx === 0){
+            Math.random() < 0.5 ? this.turnLeft() : this.turnRight()
+        }
     }
 }
